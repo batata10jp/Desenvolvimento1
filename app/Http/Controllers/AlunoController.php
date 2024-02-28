c775e7a82b241ea1299db5f6b5c6c587fcfec077
batata10jp <129328941+batata10jp@users.noreply.github.com>
2024-02-28 17:29:24 -0300

    Add files via upload

---
+++ app/Http/Controllers/AlunoController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aluno;
+use Illuminate\Http\Request;
+
+class AlunoController extends Controller
+{
+    public function index()
+    {
+        $dados = Aluno::all();
+        //dd($dados);
+
+        return view("aluno.list", ["dados" => $dados]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+
+        return view("aluno.form");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //app/http/Controller
+        Aluno::create(
+            [
+                'nome' => $request->nome,
+                'telefone' => $request->telefone,
+                'cpf' => $request->cpf,
+            ]
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $dado = Aluno::findOrFail($id);
+        $dado->delete();
+
+        return redirect('aluno');
+    }
+
+    public function search(Request $request)
+    {
+        if(!empty($request->nome)) {
+        $dados = Aluno::where(
+            "nome",
+            "like",
+            "%" . $request->nome . "%"
+        )->get();
+        } else{
+        //dd($dados);
+        $dados=Aluno::all();
+        }
+        return view("aluno.list", ["dados" => $dados]);
+    }
+}
---
+++ resources/views/aluno/form.blade.php
@@ -0,0 +1,41 @@
+@extends('base')
+@section('conteudo')
+
+    <form action="" method="post">
+
+        @csrf
+
+        <label for="">Nome</label><br>
+        <input type="text" name="nome"><br>
+
+        <button type="submit">Buscar</button>
+        <button><a href="{{ url('aluno/create') }}">Novo</a></button>
+    </form>
+
+<hr>
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Telefone</th>
+            <th>CPF</th>
+            <th colspan="2">Ações</th>
+            <th colspan="2">Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($dados as $item)
+            <tr>
+                <td>{{$item->id}}</td>
+                <td>{{$item->nome}}</td>
+                <td>{{$item->telefone}}</td>
+                <td>{{$item->cpf}}</td>
+                <td>Editar</td>
+                <td><a href="{{route('aluno.destroy',$item->id)}}">Excluir</a></td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@stop
